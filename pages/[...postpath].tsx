cda91fa4d9d2f0c3281c088e1f56987710ebef7e
Tuazor <Nabomtuazor@gmail.com>
2023-06-08 10:03:17 +0100

    Update [...postpath].tsx
    When making changes to the code, the URL redirection issue was identified where the masked URL from Facebook was appending "graphql" in front of the path, leading to a 404 error.
    
    Solution:
    To fix the URL redirection issue, the following changes were made to the code:
    
    In the getServerSideProps function, the destination property of the redirect object was modified.
    The replacement of "/graphql/" in the endpoint string was removed.
    Instead, the path was directly appended to the endpoint string.

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -14,15 +14,14 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
 	// redirect if facebook is the referer or request contains fbclid
 	if (referringURL?.includes('facebook.com') || fbclid) {
-		return {
-			redirect: {
-				permanent: false,
-				destination: `${
-					endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)
-				}`,
-			},
-		};
-	}
+  return {
+    redirect: {
+      permanent: false,
+      destination: `${endpoint}/${encodeURI(path as string)}`,
+    },
+  };
+}
+
 	const query = gql`
 		{
 			post(id: "/${path}/", idType: URI) {
